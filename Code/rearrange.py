1358c6f1c65c23381d54714fcf1f8a80b3f93967
Ricardo Rodriguez <rr092293@gmail.com>
2019-04-03 14:34:26 -0700

    Added comments to rearrange module

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,14 +1,19 @@
 import random
 import sys
 
+# Could use random.shuffle() to shuffle the order of the argument list, but decided to work it out instead.
+
 words = []
 randomized_words = []
 
+# This function appends the arguments provided into the words list
 def populate_words_list():
     for index in range(1, len(sys.argv)):
         words.append(sys.argv[index])
     return
 
+# This function chooses a random word from the words list,
+# then appends it to the randomized_words list, before removing it from the words list
 def rearrange_words():
     for index in range(0, len(words)):
         rand_index = random.randint(0, len(words) - 1)
